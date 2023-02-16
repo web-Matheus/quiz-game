2a3cbf34b3315cdac307072e223b83b0d3643585
web-Matheus <matheussantosjere02@gmail.com>
2023-02-16 16:46:20 -0300

    template pronto

---
+++ src/components/Questions.jsx
@@ -0,0 +1,15 @@
+export default function Questions() {
+  return (
+    <div className="QeA">
+        <div className="question">
+            Qual é 5 * 5 ?
+        </div>
+        <div className="answers">
+            <div className="answer wrong">10</div>
+            <div className="answer">10</div>
+            <div className="answer">10</div>
+            <div className="answer">10</div>
+        </div>
+    </div>
+  )
+}
